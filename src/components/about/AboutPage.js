99229ade9492916bf2102428e6c1fe58668ca1d8
polakto <polakto@gmail.com>
2017-04-15 17:34:13 +0200

    initial cats, antds UI - working imports

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { DatePicker } from 'antd';
 import 'antd/dist/antd.css';
+import { Steps } from 'antd';
+const Step = Steps.Step;
 
 class AboutPage extends React.Component {
   render() {
@@ -9,6 +11,12 @@ class AboutPage extends React.Component {
         <h1>About</h1>
         <p>coming soon!</p>
         <DatePicker />
+        <br/><br/><br/>
+        <Steps current={1}>
+			<Step title="Finished" description="This is a description." />
+			<Step title="In Progress" description="This is a description." />
+			<Step title="Waiting" description="This is a description." />
+		</Steps>
       </div>
     );
   }
